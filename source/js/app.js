b133984777b9282e76eaf7ae40f27c738d9e871f
Alexander Egorichev <alexchev92@gmail.com>
2016-12-11 22:25:16 +0300

    Добавил flip-анимацию на главной странице

--- source/js/app.js
+++ source/js/app.js
@@ -1,7 +1,29 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  setTimeout(function() {
-    document.querySelector('.greating_picture').classList.add('m--show');
-  }, 1000);
+    //------ Flip -------//
+
+    (function flip() {
+        $(window).on('click keydown', function (evt) {
+            var authBtn = $('.auth__btn'),
+                flipWindow = $('.auth-window'),
+                target = evt.target;
+
+            if(target.classList.contains('auth__btn')) {
+                evt.preventDefault();
+                flipWindow.addClass('auth-window--flip');
+                authBtn.animate({opacity: 0}, 400).css({'visibility': 'hidden'});
+            } else {
+                if(target.id === 'backflip' || !target.closest('.auth-window__signin') || evt.keyCode == 27)  {
+                    flipWindow.removeClass('auth-window--flip');
+                    authBtn.animate({opacity: 1}, 400).css({'visibility': 'visible'});
+                }
+            }
+        });
+
+
+
+    })();
+
+
 })();
